f6c1ff03ca6217eaf36b02e1599921cd8abb82be
minji_105 <ajhjmj9@naver.com>
2025-06-17 13:34:10 +0900

    ✨ Feat: 영화 목록 렌더링
    영화 데이터를 MovieCard 컴포넌트로 전달하여 영화 목록 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import MovieCard from "./components/MovieCard";
+import movieListData from "./data/movieListData.json";
 
 function App() {
-  return <></>;
+  const [movieList, setMovieList] = useState(movieListData);
+
+  return (
+    <div className="flex flex-wrap justify-center gap-4">
+      {movieList.results.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          title={movie.title}
+          avg={movie.vote_average}
+          imgSrc={movie.backdrop_path}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
