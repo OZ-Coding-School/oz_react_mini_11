da29910346a106b302babf6c1f86414f6ed5c7cb
minji_105 <ajhjmj9@naver.com>
2025-06-18 03:24:57 +0900

    ✨ Feat: Home에 Banner 컴포넌트 등록 및 스타일 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,30 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import movieListData from "../data/movieListData.json";
+import Banner from "../components/Banner";
 import MovieCard from "../components/MovieCard";
 
 function Home() {
-  const [movieList, setMovieList] = useState(movieListData);
+  const [movieList, setMovieList] = useState(movieListData.results);
+
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {movieList.results.map((movie) => (
-        <Link to="/details">
-          <MovieCard
-            key={movie.id}
-            title={movie.title}
-            avg={movie.vote_average}
-            imgSrc={movie.poster_path}
-          />
-        </Link>
-      ))}
-    </div>
+    <>
+      <Banner movie={movieList[6]} />
+      <div
+        className="flex flex-wrap gap-8 absolute z-20 px-[5vw] pb-20
+      bg-[linear-gradient(to_bottom,_rgba(0,0,0,0)_0%,_rgba(0,0,0,5)_30%,_rgba(0,0,0,1)_100%)]"
+      >
+        {movieList.map((movie) => (
+          <Link to="/details" key={movie.id}>
+            <MovieCard
+              title={movie.title}
+              avg={movie.vote_average}
+              imgSrc={movie.poster_path}
+            />
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
 
