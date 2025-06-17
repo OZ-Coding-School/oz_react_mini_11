df30ebda88d50acf3d231f91e8ab7bb7aca6632f
people9953 <people99532@gmail.com>
2025-06-17 14:13:00 +0900

    feat : 영화 카드 레이아웃 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import MovieCard from "./components/MovieCard";
 import movieListData from "./data/movieListData.json";
-import MovieDetail from "./components/MovieDetail";
 import "./App.css";
 
 function App() {
-  const [Movies] = useState(movieListData.results);
+  const [movies] = useState(movieListData.results);
 
   return (
-    <>
-      <div className="movie=list">
-        {Movies.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            title={movie.title}
-            poster_path={movie.poster_path}
-            vote_average={movie.vote_average}
-          />
-        ))}
-        <MovieDetail />
-      </div>
-    </>
+    <div className="movie-grid">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
   );
 }
 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { IMAGE_BASE_URL } from "../constants/imageBaseUrl";
-import "./MovieCard.css";
+
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 function MovieCard({ movie }) {
   return (
     <div className="movie-card">
       <img src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt={movie.title} />
       <h3>{movie.title}</h3>
-      <p>평점 : {movie.vote_average}</p>
+      <p>평점: {movie.vote_average}</p>
     </div>
   );
 }
