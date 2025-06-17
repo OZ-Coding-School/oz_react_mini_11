a6cd15b9be7307989e538eaf607fd695c9513d30
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-17 12:14:34 +0900

    💄 Design: breakpoint에 따라 grid items 개수 조절

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,29 @@
 import styled from "@emotion/styled";
 
+// const breakPoints = {
+//   desktop: 1440,
+//   laptop: 1024,
+//   tablet: 768,
+//   mobile: 480,
+// }
+
 const Container = styled.div`
   display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 0.5rem;
+
+  @media (max-width: 1440px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 480px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 const CardWrapper = styled.div`
