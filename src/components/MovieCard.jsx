2e9b73a618ee43d831ba608863b335e3bea225d6
dirage1 <dirage4635@gmail.com>
2025-06-17 17:45:19 +0900

    feat: MovieCard 이미지 클릭시에 detail 경로로 이동하도록 설정

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 export function MovieCard({ title, poster, rating }) {
   return (
     <div>
-      <img src={`${IMAGE_BASE_URL}${poster}`} alt={`${title} poster`} />
+      <Link to='/detail'> 
+      {/* 요구사항: 더미데이터라 개별 링크 미지정 */}
+        <img src={`${IMAGE_BASE_URL}${poster}`} alt={`${title} poster`} />
+      </Link>
       <h2>{title}</h2>
       <p>⭐ Rating: {rating}</p>
     </div>
