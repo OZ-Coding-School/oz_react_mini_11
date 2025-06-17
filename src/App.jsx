07ebdd24e372b815685f9945a9ae316713e93529
ElviaChoi <elviachoi924@gmail.com>
2025-06-17 16:11:49 +0900

    feat: 🧭 Layout과 NavBar 컴포넌트 구현 (상단 고정 UI)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import MovieList from "./pages/MovieList";
 import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MovieList />} />
-      <Route path="/details" element={<MovieDetail />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/details" element={<MovieDetail />} />
+      </Route>
     </Routes>
   );
 }
