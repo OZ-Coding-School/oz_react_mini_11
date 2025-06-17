90f15b7b9200577a76c3c99dbbaec6bec3e2eb39
minji_105 <ajhjmj9@naver.com>
2025-06-17 22:01:40 +0900

    ✨ Feat: Nav에 트랜지션 효과 추가

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 function NavBar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="flex justify-between p-4 bg-blue-300">
-      <h1>OZ 무비</h1>
+    <div
+      className={`flex justify-between items-center fixed z-[100] w-full px-[5vw] py-4 trnasition-all duration-500
+                  ${isScrolled ? "bg-black" : "bg-transparent"}`}
+    >
+      <Link to="/">
+        <h1 className="text-xl font-bold">OZMOVIE</h1>
+      </Link>
       <input type="text" className="bg-white" />
       <div>
         <button className="mr-2">로그인</button>
