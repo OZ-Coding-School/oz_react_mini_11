6f33b5d17bf1d2c5eb5bc418bfd9f957a8bceff0
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-17 13:36:45 +0900

    feat: MovieCard component

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,21 +1,40 @@
 import type { MovieData } from "../types";
+import Star from "./icons/Star";
 
 interface MovieCardProps {
   movie: MovieData;
 }
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+const STAR_STANDARDS = [1, 2, 3, 4, 5];
 
 export default function MovieCard({ movie }: MovieCardProps) {
+  const stars = movie.vote_average / 2;
+
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col p-2  shadow-2xl transition-transform hover:scale-105 space-y-1 rounded-xl">
       <img
         src={`${IMAGE_BASE_URL}/${movie.poster_path}`}
         alt={`${movie.title}-poster`}
         width={200}
+        className="w-[200px] h-[300px] object-cover object-center rounded-xl"
       />
-      <span>{movie.title}</span>
-      <span>{`${movie.vote_average.toFixed(1)}/10`}</span>
+      <span className="text-lg">{movie.title}</span>
+      <div className="flex items-center justify-end space-x-1">
+        {STAR_STANDARDS.map((standard) => {
+          let fillAmount: "full" | "half" | "none";
+
+          if (stars < standard - 0.5) {
+            fillAmount = "none";
+          } else if (stars < standard) {
+            fillAmount = "half";
+          } else {
+            fillAmount = "full";
+          }
+
+          return <Star fillAmount={fillAmount} />;
+        })}
+      </div>
     </div>
   );
 }
