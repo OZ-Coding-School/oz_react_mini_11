7b2151ab246c66964d939aae72b3b67b89e7be7b
minji_105 <ajhjmj9@naver.com>
2025-06-17 17:08:24 +0900

    ✨ Feat: 메인 화면 Home 컴포넌트로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import movieListData from "./data/movieListData.json";
 import Layout from "./components/Layout";
-import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
 
 function App() {
-  const [movieList, setMovieList] = useState(movieListData);
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={
-              <div className="flex flex-wrap justify-center gap-4">
-                {movieList.results.map((movie) => (
-                  <Link to="/details">
-                    <MovieCard
-                      key={movie.id}
-                      title={movie.title}
-                      avg={movie.vote_average}
-                      imgSrc={movie.backdrop_path}
-                    />
-                  </Link>
-                ))}
-              </div>
-            }
-          />
+          <Route index element={<Home />} />
           <Route path="details" element={<MovieDetail />} />
         </Route>
       </Routes>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import movieListData from "../data/movieListData.json";
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+  const [movieList, setMovieList] = useState(movieListData);
+  return (
+    <div className="flex flex-wrap justify-center gap-4">
+      {movieList.results.map((movie) => (
+        <Link to="/details">
+          <MovieCard
+            key={movie.id}
+            title={movie.title}
+            avg={movie.vote_average}
+            imgSrc={movie.backdrop_path}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
