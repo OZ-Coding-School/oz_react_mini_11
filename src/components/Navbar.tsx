5a97c5341bc553da4ff3ba5d60e201907d049907
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-17 14:12:58 +0900

    feat: link to home page when click logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router";
 import Button from "./Button";
 
 export default function Navbar() {
   return (
     <header className="flex items-center justify-between absolute top-0 w-full text-neutral-50 space-x-5 p-2">
-      <h1 className="text-4xl font-semibold">Movie Wiki</h1>
+      <Link to="/" className="text-4xl font-semibold">
+        Movie Wiki
+      </Link>
       <input className="bg-neutral-50 text-neutral-900 rounded-full focus:outline-none p-1 flex-1 max-w-2xl" />
       <div className="flex items-center justify-center space-x-2">
         <Button theme="outline">회원가입</Button>
