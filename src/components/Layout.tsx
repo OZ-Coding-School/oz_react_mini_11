341dfc47b204dac565943c6c3f6e9769bb01716e
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-17 14:56:38 +0900

    feat: responsive design to Navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,9 +3,9 @@ import Navbar from "./Navbar";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="bg-neutral-900">
       <Navbar />
-      <main className="bg-neutral-900 p-10 pt-16">
+      <main className="p-10 pt-54 md:pt-26">
         <Outlet />
       </main>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,11 @@ import Button from "./Button";
 
 export default function Navbar() {
   return (
-    <header className="flex items-center justify-between absolute top-0 w-full text-neutral-50 space-x-5 p-2">
+    <header className="flex flex-col items-start justify-between absolute top-0 w-full text-neutral-50 gap-5 p-5 bg-neutral-900 border-b-2 border-neutral-700 md:flex-row md:items-center">
       <Link to="/" className="text-4xl font-semibold">
         Movie Wiki
       </Link>
-      <input className="bg-neutral-50 text-neutral-900 rounded-full focus:outline-none p-1 flex-1 max-w-2xl" />
+      <input className="bg-neutral-50 text-neutral-900 rounded-full focus:outline-none p-1 w-[90%] md:flex-1 md:max-w-2xl" />
       <div className="flex items-center justify-center space-x-2">
         <Button theme="outline">회원가입</Button>
         <Button theme="default">로그인</Button>
