bc69be8a2c63b9de081128869209619dacbaa64d
dirage1 <dirage4635@gmail.com>
2025-06-17 16:44:26 +0900

    feat: 상세 페이지 레이아웃 요소 추가

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,3 +1,26 @@
+import { useState } from 'react';
+import MovieDetailData from '../data/movieDetailData.json';
+
 export function MovieDetail() {
-  return <></>;
+  const [detail, setDetail] = useState(MovieDetailData);
+  const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+  return (
+    <>
+      <img src={`${IMAGE_BASE_URL}${detail.poster_path}`} alt={detail.title} />
+      <div>
+        <h2> 제목 : {detail.title} </h2>
+      </div>
+      <div>
+        <h3> 평점 : {detail.vote_average}</h3>
+      </div>
+      <div>
+        장르 :
+        {detail.genres.map(genre => (
+          <span key={genre.id}> {genre.name} </span>
+        ))}
+      </div>
+      <div> 줄거리 : {detail.overview} </div>
+    </>
+  );
 }
