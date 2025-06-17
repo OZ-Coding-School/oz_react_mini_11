26c557636d264efdc48e5d22512df63dfa17451f
minji_105 <ajhjmj9@naver.com>
2025-06-17 16:34:13 +0900

    💄 Style: 영화 카드 크기 수정

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function MovieCard({ title, avg, imgSrc }) {
   const BASE_URL = "https://image.tmdb.org/t/p/w500/";
   return (
-    <div className="w-30">
+    <div className="w-[120px]">
       <img
         className="aspect-[0.7] object-cover"
         src={`${BASE_URL}${imgSrc}`}
