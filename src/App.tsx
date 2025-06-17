b659f226ddcd16c85ff92176251d6f6b0e8e6af3
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-17 11:42:06 +0900

    chore: install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from "react-router";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<div>home</div>} />
+      <Route path="/details/:id" element={<div>details</div>} />
+    </Routes>
+  );
 }
 
 export default App;
