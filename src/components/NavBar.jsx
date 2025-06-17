cedcb45bf199e8b215545f32b7e8fae20616caec
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-17 15:10:08 +0900

    ✨ Feat: NavBar 컴포넌트 추가
    웹사이트 타이틀, 검색바, 로그인 회원가입 버튼 등 포함

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,89 @@
+import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const Title = styled.div`
+  font-size: 3rem;
+  font-weight: 700;
+  color: #82d1f8;
+  cursor: pointer;
+`;
+
+const InputWrapper = styled.div`
+  width: 500px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 2rem;
+  border: 0;
+  border-radius: 2rem;
+  background-color: #e1f6ff;
+  font-size: 1.2rem;
+  outline: 0;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  border: 3px solid #abdef8;
+  border-radius: 1rem;
+  background-color: #fff;
+  color: #82d1f8;
+  font-size: 1rem;
+  font-weight: 500;
+  cursor: pointer;
+
+  &:hover {
+    color: #fff;
+    background-color: #abdef8;
+  }
+`;
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 100px;
+  padding: 1rem 10rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  box-shadow: 0 0 10px 3px #00000015;
+  background-color: #fff;
+`;
+
+function NavBar() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title onClick={handleClick}>Movieflix</Title>
+        <InputWrapper>
+          <Input />
+        </InputWrapper>
+      </Wrapper>
+      <ButtonWrapper>
+        <Button>로그인</Button>
+        <Button>회원가입</Button>
+      </ButtonWrapper>
+    </Container>
+  );
+}
+
+export default NavBar;
