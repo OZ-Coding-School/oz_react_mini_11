987a9b43ef374754a1486b7500c87ffb7095e2fc
dirage1 <dirage4635@gmail.com>
2025-06-17 18:44:50 +0900

    design: tailwindcss로 NavBar Css 입력

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,39 @@ import { Link } from "react-router-dom"
 
 function NavBar() {
     return(<>
-        <nav className="">
-            <Link to="/"><h3>OZ 무비</h3></Link>
-            <input type="text" placeholder="검색..."/>
-            <Link to="/login"><div>로그인</div></Link>
-            <Link to="/signin"><div>회원가입</div></Link>
+        <nav className="flex bg-black bg-contain justify-around">
+            <Link to="/"><h3 className="
+              bg-lime-100
+                font-extrabold
+                text-5xl
+                text-violet-900
+                rounded-xs
+                bg-cover
+                m-[5px]
+                p-3
+            ">OZ 무비</h3></Link>
+            <input type="text" placeholder="검색..." className="
+              bg-white
+                m-[15px]
+                rounded-s
+                w-[60%]
+            "/>
+            <Link to="/login"><div className="
+                bg-lime-200
+                text-violet-900
+                font-semibold
+                text-3xl
+                mt-[11px]
+                p-3
+            ">로그인</div></Link>
+            <Link to="/signin"><div className="
+             bg-lime-200
+                text-violet-900
+                font-semibold
+                text-3xl
+                mt-[11px]
+                p-3
+            ">회원가입</div></Link>
         </nav>
         </>)
 }
