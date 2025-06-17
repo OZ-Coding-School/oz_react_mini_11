45ada6b3cb79bdd4a82df747b80449fff159f33f
minji_105 <ajhjmj9@naver.com>
2025-06-18 03:22:58 +0900

    ✨ Feat: 상세 페이지 수정

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -6,24 +6,58 @@ function MovieDetail() {
   const [movie, setMovie] = useState(movieDetailData);
 
   return (
-    <div className="flex gap-4">
-      <img
-        className="w-1/2"
-        src={`${baseUrl}${movie.poster_path}`}
-        alt={movie.title}
+    <div className="relative overflow-hidden">
+      <div
+        className="absolute inset-0 bg-cover bg-center blur-2xl scale-[3] brightness-75"
+        style={{
+          backgroundImage: `url(${baseUrl}${movie.poster_path})`,
+        }}
       />
 
-      <div className="grid grid-rows-[1fr_1fr_2fr] gap-4 w-1/2">
-        <div className="flex justify-between gap-4 text-xl">
-          <p className="grow bg-amber-50">{movie.title}</p>
-          <p className="bg-amber-100">{movie.vote_average}</p>
+      <div className="relative flex flex-col md:flex-row gap-[5vw] px-[5vw] py-[80px]">
+        <img
+          className="w-1/3 max-h-[calc(100vh-120px)] object-contain object-top"
+          src={`${baseUrl}${movie.poster_path}`}
+          alt={movie.title}
+        />
+
+        <div className="flex flex-col">
+          <div className="flex justify-between gap-4">
+            <p className="grow text-6xl font-bold">{movie.title}</p>
+            <p className="flex justify-between items-end text-xl">
+              {movie.vote_average}&nbsp;&nbsp;
+              <span className="text-yellow-500">★</span>
+            </p>
+          </div>
+          <div className="flex gap-4 mt-3 text-gray-300">
+            <p>{movie.release_date.split("-")[0]}</p>
+            <p>|</p>
+            <p>{movie.origin_country}</p>
+            <p>|</p>
+            <p>All</p>
+          </div>
+
+          <div className="flex my-10">
+            <p className="w-[100px] text-gray-300">장르</p>
+            <div className="flex">
+              {movie.genres.map((el) => el.name).join(", ")}
+            </div>
+          </div>
+
+          <p className="w-fit mb-4 border-solid border-b-4 border-red-600 text-xl leading-9">
+            OVERVIEW
+          </p>
+          <p className="leading-6 break-keep">{movie.overview}</p>
+
+          <p className="w-fit mt-10 mb-4 border-solid border-b-4 border-red-600 text-xl leading-9">
+            STEEL CUT
+          </p>
+          <img
+            className="md:w-[300px]"
+            src={`${baseUrl}${movie.backdrop_path}`}
+            alt={movie.title}
+          />
         </div>
-        <div className="flex gap-4 bg-amber-300 text-xl">
-          {movie.genres.map((genre) => (
-            <p key={genre.id}>{genre.name}</p>
-          ))}
-        </div>
-        <p className="bg-amber-500 text-xl">{movie.overview}</p>
       </div>
     </div>
   );
