43265c21ec15cf029a3dde2409b2e0871fac5fc6
bin00125 <ebin00125@gmail.com>
2025-06-17 13:39:06 +0900

    chore:react-router-dom설치 및 구조세팅

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,29 @@ import movieListData from "./data/movieListData.json";
 import movieDetailData from "./data/movieDetailData.json";
 import "./App.css";
 import MovieCard from "./component/MovieCard";
+import { Routes, Route } from "react-router-dom";
+import Detail from "./component/Detail";
 
 function App() {
   const movieList = movieListData.results; //json객체안에 results키값을 꺼내옴, 배열형태(movieList는 영화 객체들이 들어 있는 배열)
   return (
     <>
-      <div>
-        {/* movieList 배열을 map() 함수로 하나씩 돌면서 MovieCard 컴포넌트를 렌더링
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              {/* movieList 배열을 map() 함수로 하나씩 돌면서 MovieCard 컴포넌트를 렌더링
             각 영화 데이터(movie)를 props로 넘겨줌
             key 속성은 React가 각 요소를 구분하기 위해 필요 */}
-        {movieList.map((movie) => {
-          return <MovieCard key={movie.id} movie={movie} />; //props
-        })}
-      </div>
+              {movieList.map((movie) => {
+                return <MovieCard key={movie.id} movie={movie} />; //props
+              })}
+            </div>
+          }
+        />
+        <Route path="/details" element={<Detail />} />
+      </Routes>
     </>
   );
 }
--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function MovieCard({ movie }) {
+  const baseUrl = "https://image.tmdb.org/t/p/w500";
   return (
     <>
-      <div>
-        <h2>{movie.title}</h2>
-        <p>{movie.vote_average}</p>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-      </div>
+      <Link to="/details">
+        <div>
+          <h2>{movie.title}</h2>
+          <p>{movie.vote_average}</p>
+          <img src={`${baseUrl}${movie.poster_path}`} />
+        </div>
+      </Link>
     </>
   );
 }
