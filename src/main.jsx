52105c8453fa41e1726c1b092d7f847c0d24605c
dirage1 <dirage4635@gmail.com>
2025-06-17 18:08:27 +0900

    feat: Layout 컴포넌트 생성 후 NavBar 호출, outlet을 사용하여 표시될 수 있도록 설정

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import './index.css';
 import App from './App.jsx';
 import MovieDetail from './components/MovieDetail.jsx'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layout from './components/Layout'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App/>}/>
-      <Route path='/detail' element={<MovieDetail/>}/>
-      <Route path='*' element={<h3> 404 Not Found </h3>}/>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<App/>}/>
+        <Route path='/detail' element={<MovieDetail/>}/>
+        <Route path='*' element={<h3> 404 Not Found </h3>}/>
+      </Route>
     </Routes> 
   </BrowserRouter>
 );
