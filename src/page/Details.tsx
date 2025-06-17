c1a21e9f26c049ac6db5781e4e350b1d0e80e783
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-17 15:09:30 +0900

    design: Details page

--- src/page/Details.tsx
+++ src/page/Details.tsx
@@ -1,6 +1,7 @@
 //실제 데이터 사용 시 주석 풀고 사용
 //import { useParams } from "react-router";
 import movieData from "../assets/data/movieDetailData.json";
+import StarPoints from "../components/StarPoints";
 import type { MovieDetailData } from "../types";
 
 const movie = movieData as MovieDetailData;
@@ -10,19 +11,26 @@ export default function Details() {
   //실제 데이터 사용 시 주석 풀고 사용
   //const { id } = useParams();
   return (
-    <div className="text-neutral-100 flex">
+    <div className="text-neutral-100 w-full flex justify-center space-y-10 flex-col items-center md:flex-row md:items-start md:space-x-5">
       <img
         src={`${IMAGE_BASE_URL}/${movie.poster_path}`}
         alt={`${movie.title}-poster`}
+        width={400}
+        className="w-[400px] max-h-[600px] object-cover object-center rounded"
       />
-      <div>
-        <div>
-          <span>{movie.title}</span>
-          <span>{movie.vote_average.toFixed(1)} / 10</span>
+      <div className="max-w-[600px] space-y-2">
+        <div className="flex justify-between items-center">
+          <h1 className="font-bold text-5xl">{movie.title}</h1>
+          <StarPoints point={movie.vote_average} />
         </div>
-        <div>
+        <div className="space-x-1">
           {movie.genres.map((genre) => (
-            <span key={genre.id}>{genre.name}</span>
+            <span
+              className="text-sm px-2 py-1 rounded-full bg-neutral-600"
+              key={genre.id}
+            >
+              {genre.name}
+            </span>
           ))}
         </div>
         <p>{movie.overview}</p>
