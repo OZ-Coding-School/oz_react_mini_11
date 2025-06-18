15a95959de781b36f4e2ddb6b4547d198fcae1c7
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-18 15:51:38 +0900

    💄 Design: 사이트 title용 폰트 추가 및 전체 배경색 스타일 지정

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -6,6 +6,13 @@ function GlobalStyles() {
     <Global
       styles={css`
         @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css");
+        @font-face {
+          font-family: "Tenada";
+          src: url("https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2210-2@1.0/Tenada.woff2")
+            format("woff2");
+          font-weight: normal;
+          font-style: normal;
+        }
 
         * {
           box-sizing: border-box;
@@ -18,6 +25,7 @@ function GlobalStyles() {
           font-size: 16px;
           font-family: "Pretendard Variable", Pretendard, -apple-system,
             system-ui, Roboto, "Noto Sans KR", sans-serif;
+          background: #1b1b1b;
         }
       `}
     />
