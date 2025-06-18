4c0d79e04d53655929d44a35c75be8152e7edc5d
minji_105 <ajhjmj9@naver.com>
2025-06-18 21:42:06 +0900

    ✨ Feat: Add에 Search 페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="details/:id" element={<MovieDetail />} />
+          <Route path="search" element={<Search />} />
         </Route>
       </Routes>
     </BrowserRouter>
