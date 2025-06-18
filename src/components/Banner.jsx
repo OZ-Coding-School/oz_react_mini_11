fd8aa3f1dbde4badbba3b826b3376e6049894e05
minji_105 <ajhjmj9@naver.com>
2025-06-19 01:22:03 +0900

    ✨ Feat: 배너 컴포넌트 코드 리팩토링 및 레이아웃 개선

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -34,20 +34,20 @@ function Banner() {
   }, []);
 
   useEffect(() => {
+    const changeMovie = (newMovie) => {
+      setIsVisible(false);
+      setTimeout(() => {
+        setcurrontIndex(newMovie % movieList.length);
+        setIsVisible(true);
+      }, 300);
+    };
+
     const interval = setInterval(() => {
       changeMovie(currontIndex + 1);
     }, 5000);
 
     return () => clearInterval(interval);
   }, [currontIndex, movieList.length]);
-
-  const changeMovie = (newMovie) => {
-    setIsVisible(false);
-    setTimeout(() => {
-      setcurrontIndex(newMovie % movieList.length);
-      setIsVisible(true);
-    }, 300);
-  };
 
   return !loading ? (
     <CSSTransition
@@ -57,7 +57,7 @@ function Banner() {
       unmountOnExit
       nodeRef={nodeRef}
     >
-      <div ref={nodeRef} className="relative mb-10 aspect-[2.1]">
+      <div ref={nodeRef} className="relative aspect-[2]">
         <div className="fixed w-full aspect-[2] z-0">
           <img
             className="w-full aspect-[2] object-cover "
@@ -65,25 +65,37 @@ function Banner() {
             alt={movie.title}
           />
           <div
-            className="absolute inset-0 
+            className="absolute inset-0
                       bg-[linear-gradient(to_bottom,_rgba(0,0,0,0.7)_0px,_rgba(0,0,0,0.3)_80px,_rgba(0,0,0,0.3)_calc(100%-80px),_rgba(0,0,0,1)_100%)]"
           />
         </div>
 
-        <div
-          className="flex gap-[5vw] absolute bottom-5 z-20 w-[calc(100% - 10vw)] mx-[5vw]
-                  md:w-3/4 lg:w-4/5"
-        >
-          <img
-            className="w-1/4 rounded-md"
-            src={`${BASE_URL}${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <div className="flex flex-col gap-[3vw] text-xs lg:text-sm">
-            <p className="text-gray-200">{movie.release_date.split("-")[0]}</p>
-            <p className="text-[3vw] font-black">{movie.title}</p>
-            <p className="text-gray-200 break-keep">{movie.overview}</p>
-            <p className="text-gray-200">★ {movie.vote_average.toFixed(1)}</p>
+        <div className="flex flex-col gap-6 absolute bottom-[160px] z-20 w-[calc(50%-5vw)] mx-[5vw]">
+          <p className="text-[3vw] font-black">{movie.title}</p>
+          <div>
+            <span className="mr-12">
+              ★ {movie.vote_average.toFixed(1)}&nbsp;&nbsp;|&nbsp;&nbsp;{" "}
+              {movie.vote_count}
+            </span>
+            <span className="text-gray-300">
+              {movie.original_title}&nbsp;&nbsp;·&nbsp;&nbsp;
+              {movie.release_date.split("-")[0]}
+            </span>
+          </div>
+          <p className="text-gray-300 break-keep leading-6">{movie.overview}</p>
+          <div>
+            <button
+              className="mr-4 py-3 px-8 rounded-xl bg-white text-xl text-black transition-all duration-3000
+                              hover:text-white hover:bg-blue-600"
+            >
+              ▶&nbsp;&nbsp;재생
+            </button>
+            <button
+              className="py-3 px-8 rounded-xl bg-[#90909080] text-xl transition-all duration-3000
+                              hover:bg-[#909090bb]"
+            >
+              ⓘ&nbsp;&nbsp;상세 정보
+            </button>
           </div>
         </div>
       </div>
