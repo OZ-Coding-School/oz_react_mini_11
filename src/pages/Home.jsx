a5c746a3137a8bc0d1f7af24f23134e322e0d413
jamminP <parkjaemin08@gmail.com>
2025-06-18 16:49:10 +0900

    ✨ Feat: Home.jsx 의 MovieCard API 사용하여 데이터 호출

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,18 @@ import MOVIELISTDATA from "../data/movieListData.json";
 import MovieCard from "../components/movie/MovieCard";
 import { Link } from "react-router-dom";
 import { HighRatedSwiper } from "../components/HighRatedSwiper";
+import TMDBApi from "../api/TMDM_api";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [movies, setMoives] = useState([]);
+
+  useEffect(() => {
+    TMDBApi.get("/movie/popular?language=ko-KR&page=1")
+      .then((res) => setMoives(res.data.results))
+      .catch((err) => console.error("API 호출 Error: ", err));
+  }, []);
+
   return (
     <div>
       <div className="w-screen h-100">
@@ -13,16 +23,18 @@ function Home() {
 
       <h2 className="text-2xl font-bold mb-4 pl-5">영화 목록</h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
-        {MOVIELISTDATA.results.map((movie) => (
-          <Link to={`/details`} key={movie.id}>
-            <MovieCard
-              key={movie.id}
-              title={movie.title}
-              posterPath={movie.poster_path}
-              voteAverage={movie.vote_average}
-            />
-          </Link>
-        ))}
+        {movies
+          .filter((item) => item.adult === false)
+          .map((item) => (
+            <Link to={`/details`} key={item.id}>
+              <MovieCard
+                key={item.id}
+                title={item.title}
+                posterPath={item.poster_path}
+                voteAverage={item.vote_average}
+              />
+            </Link>
+          ))}
       </div>
     </div>
   );
