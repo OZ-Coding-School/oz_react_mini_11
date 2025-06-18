8827ce88de770d128f0df967c6de39e659223c5e
minjekim64 <gimm86102@gmail.com>
2025-06-18 17:29:59 +0900

    movie

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,31 @@
 
-import { Outlet } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 
 export default function Layout() {
+  const navStyle = {
+    padding: '1rem',
+    background: '#222',
+    color: 'white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  };
+
+   const linkStyle = {
+    color: 'white',
+    textDecoration: 'none',
+    marginLeft: '1rem',
+  };
+
   return (
     <>
-      <nav style={{ padding: '1rem', background: '#222', color: 'white' }}>
+      <nav style={navStyle}>
         <h2 style={{ margin: 0 }}>🎬 Movie App</h2>
+        <div>
+          <NavLink to="/" style={linkStyle}>홈</NavLink>
+          <NavLink to="/movies" style={linkStyle}>영화 목록</NavLink>
+          <NavLink to="/favorites" style={linkStyle}>즐겨찾기</NavLink>
+        </div>
       </nav>
       <main style={{ padding: '1rem' }}>
         <Outlet />
