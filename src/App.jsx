3d362f8282b16fb4803c30f0777944ba623eb035
junjunjun13 <>
2025-06-18 15:11:21 +0900

    feat: TMDb API 연동하여 영화 목록 불러오기 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import MovieCard from './components/MovieCard';
 import MovieDetail from './components/MovieDetail';
-import movieListData from './assets/movieListData.json';
 
 export default function App() {
-  const movies = movieListData.results;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch('https://api.themoviedb.org/3/movie/popular', {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+            accept: 'application/json',
+          },
+        });
+        const data = await res.json();
+
+        // 필터링: 성인영화 제외
+        const safeMovies = data.results.filter((movie) => !movie.adult);
+        setMovies(safeMovies);
+      } catch (err) {
+        console.error('영화 데이터를 불러오는 데 실패했습니다:', err);
+      }
+    };
+
+    fetchMovies();
+  }, []);
 
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-
         <Route
           index
           element={
@@ -30,7 +50,6 @@ export default function App() {
             </div>
           }
         />
-
         <Route path="details" element={<MovieDetail />} />
       </Route>
     </Routes>
