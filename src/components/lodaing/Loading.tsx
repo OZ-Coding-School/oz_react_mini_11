e46a91bd8f3e647bba0180ff2e49d25d68c0ea68
JaeHyunLee123 <jhyon123@gmail.com>
2025-06-18 14:47:36 +0900

    feat: loading component

---
+++ src/components/lodaing/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center p-30">
+      <div className="w-16 h-16 rounded-full animate-spin bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500"></div>
+    </div>
+  );
+}
