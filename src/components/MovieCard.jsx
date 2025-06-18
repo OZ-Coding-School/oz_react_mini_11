0615bb75561db662df0f1f77f7b5db2738f1ecc7
maba <iq1359@naver.com>
2025-06-18 17:48:42 +0900

    css : 수정

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -36,7 +36,7 @@ export default function MovieCard({ id, title, rating, movieImg }) {
                             className="w-full h-full object-cover transition-all duration-300 grayscale group-hover:grayscale-0"
                         />
                     </div>
-                    <div className="p-4 flex flex-col justify-between h-20">
+                    <div className="p-4 flex flex-col justify-between h-20 opacity-50">
                         <h2 className="text-sky-50 text-lg font-semibold mb-1">{title}</h2>
                         <p className="text-amber-400 text-sm"> 평점 :{rating}</p>
                     </div>
