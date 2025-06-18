ede73b507a7a1f90d94ebfe5332b93a84eb87494
minji_105 <ajhjmj9@naver.com>
2025-06-18 14:02:41 +0900

    ✨ Feat: tmdb api 연동 및 인기 영화 목록 데이터 저장

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,43 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import movieListData from "../data/movieListData.json";
+import { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import MovieCard from "../components/MovieCard";
 
+const API_KEY = import.meta.env.REACT_APP_API_KEY;
+
 function Home() {
-  const [movieList, setMovieList] = useState(movieListData.results);
+  const [movieList, setMovieList] = useState();
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    };
+
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=ko&page=1",
+      options
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const fetchData = data.results.filter((el) => !el.adult);
+        setMovieList(fetchData);
+        console.log(fetchData);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <>
-      <Banner movie={movieList[6]} />
+      <Banner />
       <div
         className="flex flex-wrap gap-8 absolute z-20 px-[5vw] pb-20
       bg-[linear-gradient(to_bottom,_rgba(0,0,0,0)_0%,_rgba(0,0,0,5)_30%,_rgba(0,0,0,1)_100%)]"
       >
-        {movieList.map((movie) => (
+        {movieList?.map((movie) => (
           <Link to="/details" key={movie.id}>
             <MovieCard
               title={movie.title}
