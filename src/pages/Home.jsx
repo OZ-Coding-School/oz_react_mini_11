47fea896eed90a71836772c1b239f9042c47817f
minji_105 <ajhjmj9@naver.com>
2025-06-18 15:56:41 +0900

    ✨ Feat: 메인 페이지 스켈레톤 UI 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import MovieCard from "../components/MovieCard";
+import SkeletonCard from "../components/SkeletonCard";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function Home() {
   const [movieList, setMovieList] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const options = {
@@ -25,6 +27,7 @@ function Home() {
       .then((data) => {
         const fetchData = data.results.filter((el) => !el.adult);
         setMovieList(fetchData);
+        // setLoading(false);
         console.log(fetchData);
       })
       .catch((err) => console.error(err));
@@ -37,15 +40,17 @@ function Home() {
         className="flex flex-wrap gap-8 absolute z-20 px-[5vw] pb-20
       bg-[linear-gradient(to_bottom,_rgba(0,0,0,0)_0%,_rgba(0,0,0,5)_30%,_rgba(0,0,0,1)_100%)]"
       >
-        {movieList?.map((movie) => (
-          <Link to={`/details/${movie.id}`} key={movie.id}>
-            <MovieCard
-              title={movie.title}
-              avg={movie.vote_average}
-              imgSrc={movie.poster_path}
-            />
-          </Link>
-        ))}
+        {loading
+          ? Array.from({ length: 20 }).map((_, i) => <SkeletonCard key={i} />)
+          : movieList?.map((movie) => (
+              <Link to={`/details/${movie.id}`} key={movie.id}>
+                <MovieCard
+                  title={movie.title}
+                  avg={movie.vote_average}
+                  imgSrc={movie.poster_path}
+                />
+              </Link>
+            ))}
       </div>
     </>
   );
