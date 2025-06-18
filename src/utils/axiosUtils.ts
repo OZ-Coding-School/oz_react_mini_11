19f4609547118be3a8ae96e7cac62ada5eb4ac25
JaeHyunLee123 <jhyon123@gmail.com>
2025-06-18 18:00:23 +0900

    feat: util function for axios and TMDB

---
+++ src/utils/axiosUtils.ts
@@ -0,0 +1,32 @@
+import { TMDB_MOIVE_API_BASE_URL } from "../constants";
+
+export const getAxoiosTMDBMovieDetailOption = (movieId: string | number) => {
+  return {
+    method: "GET",
+    url: `${TMDB_MOIVE_API_BASE_URL}/${movieId}`,
+    params: { language: "ko" },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_READ_ACCESS_TOKEN}`,
+    },
+  };
+};
+
+type MOVIE_LIST_ORDER = "now_playing" | "popular" | "top_rated" | "upcoming";
+export const getAxoiosTMDBMovieListOption = ({
+  page = 0,
+  order,
+}: {
+  page: number;
+  order: MOVIE_LIST_ORDER;
+}) => {
+  return {
+    method: "GET",
+    url: `${TMDB_MOIVE_API_BASE_URL}/${order}`,
+    params: { language: "ko", page: `${page}` },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_READ_ACCESS_TOKEN}`,
+    },
+  };
+};
