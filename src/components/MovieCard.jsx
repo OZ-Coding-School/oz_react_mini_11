fedb0ed897f238e552dfeacbee848d1804465e08
maba <iq1359@naver.com>
2025-06-18 16:20:55 +0900

    refactor : detail page 이동 Link 로 변경

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,43 @@
-import { Navigate, useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { TMDB_IMAGE_BASE_URL } from "../constants";
 
 export default function MovieCard({ id, title, rating, movieImg }) {
     const navigate = useNavigate();
 
     return (
+        // navigate 사용으로 페이지 이동
+        // <>
+        //     <div
+        //         onClick={() => navigate(`/details/${id}`)}
+        //         className="bg-neutral-600 rounded-xl overflow-hidden
+        //         shadow-md hover:scale-105 transition-transform cursor-pointer"
+        //     >
+        //         <div className=" w-full h-64 overflow-hidden ">
+        //             <img src={`${TMDB_IMAGE_BASE_URL}${movieImg}`} alt={title} className="w-full h-full " />
+        //         </div>
+        //         <div className="p-4 flex flex-col justify-between h-20">
+        //             <h2 className="text-sky-50 text-lg font-semibold mb-1">{title}</h2>
+        //             <p className="text-amber-400 text-sm"> 평점 :{rating}</p>
+        //         </div>
+        //     </div>
+        // </>
+
+        // Link 사용으로 페이지 이동
         <>
-            <div
-                onClick={() => navigate(`/details/${id}`)}
-                className="bg-neutral-600 rounded-xl overflow-hidden 
+            <Link to={`/details/${id}`} className="black">
+                <div
+                    className="bg-neutral-600 rounded-xl overflow-hidden 
                 shadow-md hover:scale-105 transition-transform cursor-pointer"
-            >
-                <div className=" w-full h-64 overflow-hidden ">
-                    <img src={`${TMDB_IMAGE_BASE_URL}${movieImg}`} alt={title} className="w-full h-full " />
+                >
+                    <div className=" w-full h-64 overflow-hidden ">
+                        <img src={`${TMDB_IMAGE_BASE_URL}${movieImg}`} alt={title} className="w-full h-full " />
+                    </div>
+                    <div className="p-4 flex flex-col justify-between h-20">
+                        <h2 className="text-sky-50 text-lg font-semibold mb-1">{title}</h2>
+                        <p className="text-amber-400 text-sm"> 평점 :{rating}</p>
+                    </div>
                 </div>
-                <div className="p-4 flex flex-col justify-between h-20">
-                    <h2 className="text-sky-50 text-lg font-semibold mb-1">{title}</h2>
-                    <p className="text-amber-400 text-sm"> 평점 :{rating}</p>
-                </div>
-            </div>
+            </Link>
         </>
     );
 }
