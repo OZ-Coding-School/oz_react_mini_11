81e67e357de3685c7d7d3c12fc3eb9106ced7d7b
maba <iq1359@naver.com>
2025-06-19 16:31:35 +0900

    style: MovieCard컴포넌트 카드 디자인 변경

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -25,20 +25,15 @@ export default function MovieCard({ id, title, rating, movieImg }) {
         // Link 사용으로 페이지 이동
         <>
             <Link to={`/details/${id}`} className="black">
-                <div
-                    className="bg-neutral-600 rounded-xl overflow-hidden 
-                shadow-md hover:scale-105 transition-transform cursor-pointer"
-                >
-                    <div className="group w-full h-64 overflow-hidden ">
-                        <img
-                            src={`${TMDB_IMAGE_BASE_URL}${movieImg}`}
-                            alt={title}
-                            className="w-full h-full object-cover transition-all duration-300 grayscale group-hover:grayscale-0"
-                        />
-                    </div>
-                    <div className="p-4 flex flex-col justify-between h-20 opacity-50">
-                        <h2 className="text-sky-50 text-lg font-semibold mb-1">{title}</h2>
-                        <p className="text-amber-400 text-sm"> 평점 :{Number(rating).toFixed(2)}</p>
+                <div className="relative rounded-xl overflow-hidden shadow-md hover:scale-105 transition-transform cursor-pointer group">
+                    <img
+                        src={`${TMDB_IMAGE_BASE_URL}${movieImg}`}
+                        alt={title}
+                        className="w-full h-72 object-cover transition-all duration-300 grayscale group-hover:grayscale-0"
+                    />
+                    <div className="absolute bottom-0 left-0 right-0 px-4 py-3 bg-gradient-to-t from-black via-black/100 to-black/5 text-white">
+                        <h2 className="text-lg font-semibold mb-1">{title}</h2>
+                        <p className="text-xs text-amber-400 text-right">평점: {Number(rating).toFixed(2)}</p>
                     </div>
                 </div>
             </Link>
