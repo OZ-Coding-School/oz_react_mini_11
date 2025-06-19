293b7509d43b16d25c68dfbfde613123c3d3a116
minji_105 <ajhjmj9@naver.com>
2025-06-19 19:53:08 +0900

    📱 Feat: NavBar 반응형 디자인

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   Link,
   useLocation,
@@ -8,12 +8,14 @@ import {
 import useDebounce from "../hooks/useDebounce";
 
 function NavBar() {
+  const [isInputVisible, setIsInputVisible] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
   const [inputDebounce, setInputDebounce] = useState("");
   const [_, setSearchParams] = useSearchParams();
   const debouncedValue = useDebounce(inputDebounce);
   const navigate = useNavigate();
   const location = useLocation();
+  const inputRef = useRef();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -41,9 +43,11 @@ function NavBar() {
     }
   }, [debouncedValue, setSearchParams, navigate, location.pathname]);
 
-  const handleClickLogo = () => {
-    setInputDebounce("");
-  };
+  useEffect(() => {
+    if (isInputVisible && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isInputVisible]);
 
   return (
     <div
@@ -51,21 +55,62 @@ function NavBar() {
                   ${isScrolled ? "bg-black" : "bg-transparent"}`}
     >
       <Link to="/">
-        <h1 className="text-xl font-bold" onClick={handleClickLogo}>
+        <h1
+          className="text-xl font-bold md:hidden"
+          onClick={() => setInputDebounce("")}
+        >
+          OZ
+        </h1>
+        <h1
+          className="text-xl font-bold hidden md:block"
+          onClick={() => setInputDebounce("")}
+        >
           OZMOVIE
         </h1>
       </Link>
-      <input
-        type="text"
-        className="bg-white"
-        value={inputDebounce}
-        onChange={handleInputDebounce}
-      />
-      <div>
+      <div className="flex items-center gap-[2vw]">
+        <div className="flex items-center">
+          <input
+            type="text"
+            ref={inputRef}
+            className={`outline-none border-b-2 bg-transparent text-white transition transition-width duration-300 
+                        ${
+                          isInputVisible || location.pathname === "/search"
+                            ? "w-[calc(150px+5vw)]"
+                            : "w-0"
+                        }`}
+            style={{ transitionProperty: "width" }}
+            value={inputDebounce}
+            onBlur={() => setIsInputVisible(false)}
+            onChange={handleInputDebounce}
+          />
+          <button
+            onClick={() => setIsInputVisible(!isInputVisible)}
+            className={`text-white text-xl ${isInputVisible ? "hidden" : ""}`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              role="img"
+              viewBox="0 0 24 24"
+              width="20"
+              height="20"
+              data-icon="MagnifyingGlassStandard"
+              aria-hidden="true"
+              className="search-icon"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10ZM15.6177 17.0319C14.078 18.2635 12.125 19 10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C14.9706 1 19 5.02944 19 10C19 12.125 18.2635 14.078 17.0319 15.6177L22.7071 21.2929L21.2929 22.7071L15.6177 17.0319Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </button>
+        </div>
         <Link to="/login">
           <button className="mr-2">로그인</button>
         </Link>
-        <button>회원가입</button>
       </div>
     </div>
   );
