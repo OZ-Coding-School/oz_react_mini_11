91d472f0007090fac2b568a716da341e09b1de8f
Yunahha <yunah19960401@gmail.com>
2025-06-19 11:32:45 +0900

    TMdb DB API 연결

--- src/page/MovieDetail.jsx
+++ src/page/MovieDetail.jsx
@@ -1,29 +1,51 @@
 import React, {useState, useEffect} from 'react';
-import movieData from '../../data/movieDetailData.json';
+import { useParams } from 'react-router-dom';
 import './MovieDetail.css'
 
 function MovieDetail() {
-    const [movieDetail, setMovieDetail] = useState()
+    const { id } = useParams();
+    const [movieDetail, setMovieDetail] = useState(null);
 
     useEffect(() => {
-        setMovieDetail(movieData)
-    }, [])
+        const fetchMovieDetail = async () => {
+        try {
+            const res = await fetch(`https://api.themoviedb.org/3/movie/${id}`, {
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_TOKEN}`,
+            },
+            });
+
+            const data = await res.json();
+            setMovieDetail(data);
+        } catch (error) {
+            console.error('영화 상세 정보 로드 실패:', error);
+        }};
+
+        fetchMovieDetail();
+    }, [id]);
 
     if (!movieDetail) return <div>로딩 중...</div>;
 
     return (
-    <div className='movie_page'>
-        <img className='movie_img' src={`https://image.tmdb.org/t/p/w500${movieDetail.poster_path}`}/>
+        <div className='movie_page'>
+        <img
+            className='movie_img'
+            src={`https://image.tmdb.org/t/p/w500${movieDetail.poster_path}`}
+            alt={movieDetail.title}
+        />
         <div className='movie_info'>
             <div className='title_vote'>
-                <h2 className='movie_title'>{movieDetail.title}</h2>
-                <p className='movie_vote'>{movieDetail.vote_average}</p>
+            <h2 className='movie_title'>{movieDetail.title}</h2>
+            <p className='movie_vote'>{movieDetail.vote_average}</p>
             </div>
-            <p className='movie_genre'>{movieDetail.genres.map((g) => g.name).join(', ')}</p>
+            <p className='movie_genre'>
+            {movieDetail.genres.map((g) => g.name).join(', ')}
+            </p>
             <p className='movie_overview'>{movieDetail.overview}</p>
         </div>
-    </div>
-    )
-    }
+        </div>
+    );
+}
 
 export default MovieDetail;
