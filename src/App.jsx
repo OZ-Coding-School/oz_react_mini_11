fb4ff872934f535b0b62673a02f91a3875e25089
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-19 13:22:06 +0900

    ♻️ Refactor: detail 페이지로 넘어갈 때 영화 id를 파라미터로 갖도록 route 수정

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/details" element={<MovieDetail />} />
+          <Route path="/detail/:movieId" element={<MovieDetail />} />
         </Route>
       </Routes>
     </BrowserRouter>
