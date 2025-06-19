3ee5487e32c8b91ac13ea49bf8d6da08886934d0
JaeHyunLee123 <jhyon123@gmail.com>
2025-06-19 16:06:20 +0900

    feat: SearchInput features

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+import { useDebounce } from "../hooks/useDebounce";
+import { useNavigate } from "react-router";
+
 export default function SearchInput({
   className,
 }: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input className={className} />;
+  const [searchParam, setSearchParam] = useState("");
+  const debouncedParam = useDebounce(searchParam, 200);
+  const navigate = useNavigate();
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setSearchParam(event.target.value);
+  };
+
+  useEffect(() => {
+    navigate(`/search?searchParam=${debouncedParam}`);
+  }, [debouncedParam, navigate]);
+
+  return <input className={className} onChange={handleChange} />;
 }
