f5df5ed683e991702464bd602d5fe234c797970b
minji_105 <ajhjmj9@naver.com>
2025-06-20 16:41:07 +0900

    ✨ Feat: SliderSection 컴포넌트 생성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
 import Banner from "../components/Banner";
-import MovieCard from "../components/MediaCard";
 import SkeletonCard from "../components/skeletons/SkeletonCard";
+import SliderSection from "../components/SliderSection";
 import useFetch from "../hooks/useFetch";
 
 function Home() {
@@ -13,20 +12,14 @@ function Home() {
     <>
       <Banner />
       <div
-        className="flex flex-wrap gap-8 absolute z-20 mt-[-9vw] px-[5vw] pb-20
-      bg-[linear-gradient(to_bottom,_rgba(0,0,0,0)_0%,_rgba(0,0,0,5)_80px,_rgba(0,0,0,1)_100%)]"
+        className="w-full absolute z-20 mt-[-9vw] pb-20
+                    bg-[linear-gradient(to_bottom,_rgba(0,0,0,0)_0%,_rgba(0,0,0,5)_80px,_rgba(0,0,0,1)_100%)]"
       >
-        {loading
-          ? Array.from({ length: 20 }).map((_, i) => <SkeletonCard key={i} />)
-          : popularMovieList?.map((movie) => (
-              <Link to={`/details/movie/${movie.id}`} key={movie.id}>
-                <MovieCard
-                  title={movie.title}
-                  avg={movie.vote_average}
-                  imgSrc={movie.poster_path}
-                />
-              </Link>
-            ))}
+        {loading ? (
+          Array.from({ length: 20 }).map((_, i) => <SkeletonCard key={i} />)
+        ) : (
+          <SliderSection title={"인기 영화"} datas={popularMovieList} />
+        )}
       </div>
     </>
   );
