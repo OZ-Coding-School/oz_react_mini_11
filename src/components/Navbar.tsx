a8ce4004775b106367600058c5879bf8a414ff66
JaeHyunLee123 <jhyon123@gmail.com>
2025-06-20 10:27:23 +0900

    fix: remove search logic code at Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,12 @@
 import { useNavigate } from "react-router";
 import Button from "./Button";
-import { useEffect, useState } from "react";
-import { useDebounce } from "../hooks/useDebounce";
-
+import SearchInput from "./SearchInput";
 export default function Navbar() {
-  const [searchParam, setSearchParam] = useState("");
-  const debouncedParam = useDebounce(searchParam, 200);
   const navigate = useNavigate();
 
   const handleLogoClick = () => {
-    setSearchParam("");
     navigate("/");
   };
-
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setSearchParam(event.target.value);
-  };
-
-  useEffect(() => {
-    if (debouncedParam === "") {
-      navigate(`/`);
-    } else {
-      navigate(`/search?searchParam=${debouncedParam}`);
-    }
-  }, [debouncedParam, navigate]);
 
   return (
     <header className="z-50 flex flex-col items-start justify-between fixed top-0 w-full text-neutral-50 gap-5 p-5 bg-neutral-900 border-b-2 border-neutral-700 md:flex-row md:items-center">
@@ -33,10 +16,7 @@ export default function Navbar() {
       >
         Movie Wiki
       </div>
-      <input
-        onChange={handleChange}
-        className="bg-neutral-50 text-neutral-900 rounded-full focus:outline-none px-3 py-1 w-[90%] md:flex-1 md:max-w-2xl"
-      />
+      <SearchInput className="bg-neutral-50 text-neutral-900 rounded-full focus:outline-none px-3 py-1 w-[90%] md:flex-1 md:max-w-2xl" />
       <div className="flex items-center justify-center space-x-2">
         <Button theme="outline">회원가입</Button>
         <Button theme="default">로그인</Button>
