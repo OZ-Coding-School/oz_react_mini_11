1b8243e626aadad199098306f04a32c0b63a03d2
minji_105 <ajhjmj9@naver.com>
2025-06-20 16:32:16 +0900

    💄 Style: MediaCard 크기 속성 삭제

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -3,7 +3,8 @@ import { BASE_URL } from "../constant/index";
 function MediaCard({ title, imgSrc }) {
   return (
     <img
-      className="max-w-[200px] aspect-[0.7] object-cover rounded-sm"
+      className="aspect-[0.7] object-cover rounded-sm
+      p-[0.2vw] white-space-nowrap"
       src={`${BASE_URL}${imgSrc}`}
       alt={title}
     />
