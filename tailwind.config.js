32309901eac3f63cf3141f69d076b1b300588e54
heyoung <heyoung0218@gmail.com>
2025-06-20 15:06:32 +0800

    feat: 다크모드/라이트모드 상태 관리용 ThemeContext 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
     },
-    darkMode: "class",
-    plugins: [],
-  }
+  },
+  darkMode: "class",
+  plugins: [],
+};
