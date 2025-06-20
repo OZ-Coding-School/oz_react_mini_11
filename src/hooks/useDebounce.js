e32d05e95dbc14165144c734d408ab1128ae95da
heyoung <heyoung0218@gmail.com>
2025-06-20 10:43:01 +0800

    refactor: useDebounce 변수명 개선

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 
 function useDebounce(value, delay = 500) {
-    const [debounceValue, setDebounceValue] = useState(value);
+    const [debouncedValue, setDebouncedValue] = useState(value);
 
     useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebounceValue(value);
+        const timer = setTimeout(() => {
+            setDebouncedValue(value); //delay가 지나가면 값 업데이트
         }, delay);
 
-        return () => clearTimeout(handler);
+        return () => clearTimeout(timer); //value또는 delay가 바뀌기 전에 타이머 제거
     }, [value, delay]);
 
-    return debounceValue;
+    return debouncedValue;
 } 
 
 export default useDebounce;
