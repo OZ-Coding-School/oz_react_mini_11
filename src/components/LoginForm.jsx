d4b8e082af1301722b6677b181851e13f9689179
maba <iq1359@naver.com>
2025-06-20 17:21:42 +0900

    feat : 로그인 폼을 만들기 위한 LoginForm컴포넌트 생성

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,12 @@
+export default function LoginForm() {
+    return (
+        <>
+            <div className="absolute inset-0 z-10 flex items-center justify-center">
+                <div className="text-white text-3xl bg-black bg-opacity-50 px-4 py-2 rounded-md">
+                    dasdasdasd
+                    <div>asdasdasdas</div>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import styled from "styled-components";
 import LoginBackground from "../components/LoginBackground";
+import LoginForm from "../components/LoginForm";
 
 export default function Login() {
     return (
@@ -9,12 +10,7 @@ export default function Login() {
             <div className="relative w-full h-screen overflow-hidden">
                 <LoginBackground />
 
-                <div className="absolute inset-0 z-10 flex items-center justify-center">
-                    <div className="text-white text-3xl bg-black bg-opacity-50 px-4 py-2 rounded-md">
-                        dasdasdasd
-                        <div>asdasdasdas</div>
-                    </div>
-                </div>
+                <LoginForm />
             </div>
         </>
     );
