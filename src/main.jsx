3710f9ce15295da15a7c218a2dbce04418b1b98b
heyoung <heyoung0218@gmail.com>
2025-06-20 15:07:10 +0800

    feat: ThemeProvider로 전역 다크모드 설정 추가

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,18 @@ import Layout from "./pages/Layout";
 import App from "./pages/App";
 import MovieDetail from "./pages/MovieDetail";
 
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<App />} />
-        <Route path="details/:id" element={<MovieDetail />} />
-      </Route>
-    </Routes>
+    <ThemeProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="details/:id" element={<MovieDetail />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   </BrowserRouter>
 );
