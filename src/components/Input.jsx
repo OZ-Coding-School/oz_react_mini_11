9c51ae3e759132dc8d7e578afecc196befcd98f4
minji_105 <ajhjmj9@naver.com>
2025-06-22 04:59:46 +0900

    ✨ Feat: Input 컴포넌트 error 메시지 추가

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-function Input({ id, label, type, placeholder, hideLabel = false }) {
+function Input({ id, label, type, placeholder, hideLabel = false, error }) {
   return (
     <>
       {!hideLabel && <label htmlFor={id}>{label}</label>}
@@ -6,8 +6,12 @@ function Input({ id, label, type, placeholder, hideLabel = false }) {
         id={id}
         type={type}
         placeholder={placeholder}
-        className="w-full py-4 px-8 rounded-md outline-none bg-[#96969644] text-base text-white"
+        className={`w-full py-4 px-8 rounded-md outline-none bg-[#96969644] text-base text-white
+                    ${error ? 'border border-red-700':''}`}
       />
+      {error && (
+        <p className="mt-[-8px] text-sm text-red-500">{error}</p>
+      )}
     </>
   );
 }
