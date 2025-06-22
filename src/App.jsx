aad4becbb2a49f918faba5fac703dfe1bd536b3f
sysysysyb <87892147+sysysysyb@users.noreply.github.com>
2025-06-22 19:55:44 +0900

    ♻️ Refactor: login, signup 라우터 설정 추가

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ const Layout = lazy(() => import("./layout/Layout"));
 const Home = lazy(() => import("./pages/Home/Home"));
 const Search = lazy(() => import("./pages/Search/Search"));
 const MovieDetail = lazy(() => import("./components/MovieDetail/MovieDetail"));
+const Login = lazy(() => import("./pages/Login/Login"));
+const Signup = lazy(() => import("./pages/Signup/Signup"));
 
 function App() {
   return (
@@ -16,7 +18,8 @@ function App() {
         <Wrapper>
           <StyledLottie animationData={loadingAnimation} loop autoplay />
         </Wrapper>
-      }>
+      }
+    >
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -24,6 +27,8 @@ function App() {
             <Route path="/search" element={<Search />} />
             <Route path="/detail/:movieId" element={<MovieDetail />} />
           </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </Suspense>
