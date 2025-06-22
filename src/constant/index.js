f627fbf9d3be0e3664ea73a8546235ad67b05ae6
minji_105 <ajhjmj9@naver.com>
2025-06-22 17:05:11 +0900

    ✨ Feat: supabase url, key 상수 선언

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,3 +1,6 @@
 export const BASE_URL = "https://image.tmdb.org/t/p/w500";
 export const BASE_URL_ORIGIN = "https://image.tmdb.org/t/p/original";
 export const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const SUPABASE_URL = import.meta.env.VITE_PROJECT_URL;
+export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
