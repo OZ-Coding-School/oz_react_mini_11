3f00e0c86e859fc586f5c5599066d311c6a9e80f
minji_105 <ajhjmj9@naver.com>
2025-06-23 01:58:52 +0900

    ✨ Feat: Input 컴포넌트에 value 및 onChange props 추가

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,20 @@
-function Input({ id, label, type, placeholder, hideLabel = false, error }) {
+function Input({ id, label, type, value, placeholder, error, onChange, hideLabel = false }) {
   return (
     <>
-      {!hideLabel && <label htmlFor={id}>{label}</label>}
+      { !hideLabel && <label htmlFor={ id }>{ label }</label> }
       <input
-        id={id}
-        type={type}
-        placeholder={placeholder}
-        className={`w-full py-4 px-8 rounded-md outline-none bg-[#96969644] text-base text-white
-                    ${error ? 'border border-red-700':''}`}
+        id={ id }
+        type={ type }
+        value={ value }
+        placeholder={ placeholder }
+        className={ `w-full py-4 px-8 rounded-md outline-none bg-[#96969644] text-base text-white
+                    ${ error ? 'border border-red-700' : '' }` }
+        onChange={ onChange }
+        required
       />
-      {error && (
-        <p className="mt-[-8px] text-sm text-red-500">{error}</p>
-      )}
+      { error && (
+        <p className="mt-[-8px] text-sm text-red-500">{ error }</p>
+      ) }
     </>
   );
 }
