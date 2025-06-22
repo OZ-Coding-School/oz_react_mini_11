1d00bf97842de1f1c70a4c87bb82cb30c722cb16
sysysysyb <87892147+sysysysyb@users.noreply.github.com>
2025-06-22 19:57:27 +0900

    ✨ Feat: Signup 페이지 기본 구조 작성

---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: pink;
+`;
+
+function Signup() {
+  return (
+    <Container>
+      <div>Signup</div>
+    </Container>
+  );
+}
+
+export default Signup;
