b872d4d0d21c2abce720decc54aaa566b5bb8c97
minji_105 <ajhjmj9@naver.com>
2025-06-23 03:04:58 +0900

    ✨ Feat: 로그인 기능 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,72 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import supabase from "../supabaseClient";
+import LOGIN_FIELDS from '../constant/loginFields';
+import useFormReducer from "../hooks/useFormReducer";
 import AuthLayout from "../components/AuthLayout";
 import Input from "../components/Input";
 
+const initialState = {
+  email: '',
+  password: '',
+  errors: {
+    email: '',
+    password: '',
+  }
+};
+
 function Login() {
+  const { state, handleChange } = useFormReducer(initialState);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { email, password } = state;
+
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+      });
+      console.log('login success: ', data);
+
+      if (error) {
+        alert('[로그인 실패] ' + error.message);
+      } else {
+        navigate('/');
+      }
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      console.log('로그인 시도 값:', email, password);
+    }
+  };
+
   return (
     <div className="relative w-full min-h-screen h-full overflow-x-hidden">
       <AuthLayout />
 
-      <div
+      <form
         className="relative flex flex-col gap-4 w-full min-w-[340px] mt-14 mx-auto mb-20 p-[5vw] rounded-2xl bg-black
                   sm:w-[500px] sm:mt-24 sm:p-14 sm:bg-[#000000c1]"
+        onSubmit={handleLogin}
       >
         <h2 className="mb-4 text-3xl font-bold sm:text-center">로그인</h2>
-        <Input
-          id="email"
-          label="이메일"
-          type="email"
-          placeholder="이메일 주소"
-          hideLabel
-        />
-        <Input
-          id="password"
-          label="비밀번호"
-          type="password"
-          placeholder="비밀번호"
-          hideLabel
-        />
+        {LOGIN_FIELDS.map((field) => (
+          <Input
+            key={field.id}
+            id={field.id}
+            label={field.label}
+            type={field.type}
+            value={state[field.id]}
+            placeholder={field.placeholder}
+            error={state.errors[field.id]}
+            onChange={handleChange}
+            hideLabel={field.hideLabel}
+          />
+        ))}
         <button
+          type="submit"
           className="w-full py-2 px-6 rounded-md bg-red-primary hover:bg-red-hover text-lg text-white
                       transition-all duration-300"
         >
@@ -40,7 +80,7 @@ function Login() {
             <span className="hover:underline">지금 가입하세요.</span>
           </Link>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
