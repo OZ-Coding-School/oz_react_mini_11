d621bc8d0cbe0b73ea8318686c28636b2a27596f
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-23 00:24:32 +0900

    ♻️ Feat: Home 페이지에 여러 종류의 영화 목록 렌더링

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,23 @@ import "swiper/css";
 import { getPopularMovies } from "../../apis/popularMovieApi";
 import { Container } from "./Home.styles";
 import MovieList from "../../components/MovieList/MovieList";
+import { getTopRatedMovies } from "../../apis/topRatedMoviesApi";
+import { getUpcomingMovies } from "../../apis/UpcomingMoviesApi";
+import { getTrendingNowMovies } from "../../apis/trendingMoviesApi";
+import { getFantasyMovies } from "../../apis/fantasyMoviesApi";
+import { getActionMovies } from "../../apis/actionMoviesApi";
+import { getComedyMovies } from "../../apis/comedyMoviesApi";
 
 function Home() {
   return (
     <Container>
+      <MovieList api={getTrendingNowMovies} sectionTitle="Trending Now" />
       <MovieList api={getPopularMovies} sectionTitle="Popular" />
+      <MovieList api={getTopRatedMovies} sectionTitle="Top Rated" />
+      <MovieList api={getUpcomingMovies} sectionTitle="Upcoming" />
+      <MovieList api={getFantasyMovies} sectionTitle="Fantasy 인기 영화" />
+      <MovieList api={getActionMovies} sectionTitle="Action 인기 영화" />
+      <MovieList api={getComedyMovies} sectionTitle="Comedy 인기 영화" />
     </Container>
   );
 }
