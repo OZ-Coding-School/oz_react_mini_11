8f596d47a91bde58aceba09fbde205597581f4ea
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-22 22:54:59 +0900

    ✨ Feat: Signup 페이지 스타일 지정

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,6 +1,64 @@
 import styled from "@emotion/styled";
 import loginBgImage from "../../assets/images/login-bg.jpg";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import NextButton from "../../assets/images/angle-right.svg?react";
+
+export const Input = styled.input`
+  border: 1px solid #d0d0d0;
+  padding: 1.25rem;
+  align-self: stretch;
+  color: #fff;
+  background: #00000085;
+  font-size: 1rem;
+  border-radius: 0.5rem;
+  outline: none;
+
+  &::placeholder {
+    color: #d0d0d0;
+  }
+
+  &:focus {
+    outline: 3px solid #fff;
+    outline-offset: 2px;
+  }
+
+  @media (min-width: ${(props) => props.theme.breakPoints.mobile}) {
+    font-size: 1.25rem;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  background-color: ${(props) => props.theme.colors.purple.dark};
+  border-radius: 0.5rem;
+  color: #fff;
+  text-decoration: none;
+  cursor: pointer;
+  transition: 0.2s ease-out;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.purple.darkActive};
+  }
+`;
+
+const LoginLink = styled(StyledLink)`
+  padding: 0.75rem 1rem;
+  font-size: 1rem;
+`;
+
+const SignupLink = styled(StyledLink)`
+  padding: 1rem 1.5rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const StyledPrevIcon = styled(NextButton)`
+  padding-top: 2px;
+  width: 24px;
+  height: 24px;
+  fill: #eee;
+`;
 
 export const Logo = styled.div`
   width: fit-content;
@@ -21,11 +79,39 @@ export const Logo = styled.div`
   }
 `;
 
+const TextBold = styled.span`
+  font-weight: 700;
+  font-size: 2.75rem;
+`;
+
+const TextRegular = styled.span`
+  font-size: 1.25rem;
+`;
+
+const Card = styled.div`
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+  z-index: 1;
+  color: #fff;
+`;
+
+const SignupWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const LogoWrapper = styled.div`
   width: 100%;
   padding: 1.5rem 10rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   z-index: 1;
-  /* background-color: green; */
 
   @media (max-width: ${(props) => props.theme.breakPoints.laptop}) {
     padding: 2.2rem 5%;
@@ -56,7 +142,7 @@ const Container = styled.div`
       background-image: url(${loginBgImage});
       background-position: center;
       background-size: cover;
-      filter: brightness(0.5);
+      filter: brightness(0.25);
     }
   }
 `;
@@ -70,21 +156,26 @@ function Signup() {
   return (
     <Container>
       <LogoWrapper>
-        <Logo onClick={handleTitleClick}>
-          <span>MOVIE</span>
-          <span>FLIX</span>
-        </Logo>
-        <button>로그인</button>
+        <Logo onClick={handleTitleClick}>MOVIEFLIX</Logo>
+        <LoginLink to="/login" type="button">
+          로그인
+        </LoginLink>
       </LogoWrapper>
-      <div>
-        <div>영화를 무제한으로</div>
-        <div>시청할 준비가 되셨나요? 시작하려면 가입 정보를 입력하세요</div>
-        <input type="text" placeholder="이름" />
-        <input type="text" placeholder="이메일 주소" />
-        <input type="password" placeholder="비밀번호" />
-        <input type="password" placeholder="비밀번호 확인" />
-        <button>시작하기</button>
-      </div>
+      <SignupWrapper>
+        <Card>
+          <TextBold>영화를 무제한으로</TextBold>
+          <TextRegular>
+            시청할 준비가 되셨나요? 시작하려면 가입 정보를 입력하세요
+          </TextRegular>
+          <Input type="text" placeholder="이름" />
+          <Input type="text" placeholder="이메일 주소" />
+          <Input type="password" placeholder="비밀번호" />
+          <Input type="password" placeholder="비밀번호 확인" />
+          <SignupLink to="/">
+            시작하기 <StyledPrevIcon />
+          </SignupLink>
+        </Card>
+      </SignupWrapper>
     </Container>
   );
 }
