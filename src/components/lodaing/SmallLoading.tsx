f5d80479a8a8e4cd1eb3d2a3245cfe268ad64c78
JaeHyunLee123 <jhyon123@gmail.com>
2025-06-23 13:44:56 +0900

    feat: small loading component:

---
+++ src/components/lodaing/SmallLoading.tsx
@@ -0,0 +1,7 @@
+export default function SmallLoading() {
+  return (
+    <div className="flex justify-center">
+      <div className="size-8 rounded-full animate-spin bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500"></div>
+    </div>
+  );
+}
