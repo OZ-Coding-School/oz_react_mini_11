c4154028d95afb1ad613c04ccb0ec0dda18d07d8
JaeHyunLee123 <jhyon123@gmail.com>
2025-06-23 13:00:00 +0900

    design: user menu UI

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,5 +1,30 @@
-interface UserMenuProps {}
+import { useState } from "react";
+import User from "./icons/User";
 
-export default function UserMenu({}: UserMenuProps) {
-  return <div className="size-10 rounded-full bg-violet-500"></div>;
+export default function UserMenu() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsMenuVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsMenuVisible(false);
+  };
+
+  return (
+    <div
+      onMouseOver={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
+      className="size-10 rounded-full bg-violet-500 flex justify-center items-center relative hover:cursor-pointer"
+    >
+      <User className="text-neutral-100" />
+      {isMenuVisible ? (
+        <div className="absolute bg-neutral-500 text-neutral-100 flex flex-col space-y-2 w-[80px] p-2 top-10 -left-10 rounded hover:cursor-pointer">
+          <span>관심 목록</span>
+          <span>로그아웃</span>
+        </div>
+      ) : null}
+    </div>
+  );
 }
